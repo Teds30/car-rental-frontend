2cc7e6023f8c19f2d24330b5f1b4bdee44c5f7ed
Ted30 <teddymarcenaje@gmail.com>
2023-01-21 19:54:36 +0800

    styled actions

--- src/components/Navigation/TopNav/TopNav.js
+++ src/components/Navigation/TopNav/TopNav.js
@@ -3,6 +3,9 @@ import React, { useContext } from 'react'
 import { ThemeContext } from '../../../context/ThemeContext'
 import { AuthContext } from '../../../context/AuthContext'
 
+import BorderlessButton from '../../UIElements/Button/BorderlessButton'
+import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs'
+
 import styles from './TopNav.module.css'
 
 const TopNav = () => {
@@ -19,8 +22,16 @@ const TopNav = () => {
     return (
         <div className={styles['topnav']}>
             <div className={styles['col']}>
-                <button onClick={changeTheme}>Change Theme</button>
-                <button onClick={logout}>Logout</button>
+                <button onClick={changeTheme} className={styles['action']}>
+                    {themeCtx.theme === 'light' ? (
+                        <BsFillSunFill className={styles['sun']} />
+                    ) : (
+                        <BsFillMoonFill className={styles['moon']} />
+                    )}
+                </button>
+                <BorderlessButton btnType="danger" onClick={logout}>
+                    Logout
+                </BorderlessButton>
             </div>
         </div>
     )
